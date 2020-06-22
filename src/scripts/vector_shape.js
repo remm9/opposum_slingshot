fcd9d2fb5309144c8508468c6f61a881058c17bc
roman <admin@MacBook-Pro-9.local>
2020-06-22 12:09:59 -0400

    chande vector file

--- src/scripts/vector_shape.js
+++ src/scripts/vector_shape.js
@@ -1,23 +1,23 @@
 
 
-var can = document.getElementById('canvas1');
-var ctx = can.getContext('2d');
+// var can = document.getElementById('canvas1');
+// var ctx = can.getContext('2d');
 
-ctx.fillStyle = "lightgray";
-const p0 = { x: 10, y: 10 }
-const p1 = { x: 100, y: 20 }
-const p2 = { x: 110, y: 110 }
+// ctx.fillStyle = "lightgray";
+// const p0 = { x: 10, y: 10 }
+// const p1 = { x: 100, y: 20 }
+// const p2 = { x: 110, y: 110 }
 
-ctx.fillStyle = "lightgray";
+// ctx.fillStyle = "lightgray";
 
-ctx.beginPath();
-ctx.moveTo(p0.x, p0.y);
-ctx.arcTo(p1.x, p1.y, p2.x, p2.y, 100);
-ctx.lineTo(p2.x, p2.y);
-ctx.lineTo(10, 110)
-ctx.lineTo(10, 10)
-ctx.fill();
-ctx.stroke();
+// ctx.beginPath();
+// ctx.moveTo(p0.x, p0.y);
+// ctx.arcTo(p1.x, p1.y, p2.x, p2.y, 100);
+// ctx.lineTo(p2.x, p2.y);
+// ctx.lineTo(10, 110)
+// ctx.lineTo(10, 10)
+// ctx.fill();
+// ctx.stroke();
 
 
 // .quadrant {
